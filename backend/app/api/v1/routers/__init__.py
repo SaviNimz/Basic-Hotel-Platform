6b71a9e9969502bd5455c87ec204c7e3a995166d
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2026-01-17 12:23:18 +0530

    added auth router

--- backend/app/api/v1/routers/__init__.py
+++ backend/app/api/v1/routers/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.api.v1.routers import auth, users, hotels, rooms
 
 api_router = APIRouter()
-api_router.include_router(auth.router, tags=["login"])
+api_router.include_router(auth.router, prefix="/auth", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(hotels.router, tags=["hotels"])
 api_router.include_router(rooms.router, tags=["rooms"])
